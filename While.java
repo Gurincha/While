5ee581c6f63d94a7867307033f65c6b0cebf18eb
Gurincha <gustavo.rincha@fatec.sp.gov.br>
2022-03-07 19:26:02 +0000

    salvo

---
+++ While.java
@@ -0,0 +1,11 @@
+public class While{
+
+    public static void main(String[] args) {
+        
+        int x=15;
+        while (x<18){
+            System.out.println(" Voce não tem premissão para entrar");
+            x++;
+        }
+    }
+}
